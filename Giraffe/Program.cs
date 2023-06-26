bb5c0a24bde6cf06d904a670bd46e3fd5463ed3d
chk0913 <hyokeun913@naver.com>
2023-06-26 17:09:45 +0900

    test

---
+++ Giraffe/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.2;
+
+            Console.ReadLine();
+        }
+    }
+}
