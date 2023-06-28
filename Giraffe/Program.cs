861a822560d1f77bdc27864a770dd28a4bc86f21
chk0913 <hyokeun913@naver.com>
2023-06-28 11:25:54 +0900

    44

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -1,17 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
 namespace Giraffe
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string phrase = "Giraffe Academy";
-            char grade = 'A';
-            int age = 30;
-            double gpa = 3.2;
-            bool isMale = true;
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(false);
+                Console.WriteLine(num1 / num2);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
         }
