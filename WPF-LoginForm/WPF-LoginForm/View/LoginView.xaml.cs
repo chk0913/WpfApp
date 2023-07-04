5e828336d3d008a9593d10de3c4f83280abd90a5
chk0913 <hyokeun913@naver.com>
2023-07-04 17:31:49 +0900

    20230704

--- WPF-LoginForm/WPF-LoginForm/View/LoginView.xaml.cs
+++ WPF-LoginForm/WPF-LoginForm/View/LoginView.xaml.cs
@@ -39,5 +39,10 @@ namespace WPF_LoginForm.View
         {
             Application.Current.Shutdown();
         }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
